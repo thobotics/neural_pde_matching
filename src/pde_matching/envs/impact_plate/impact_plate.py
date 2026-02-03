e2ba063bd91783cfb64f55d1c3041a69ca6b7260
Tai Hoang <thobotics@gmail.com>
2026-02-03 22:47:58 +0100

    update dataset path for configs

--- src/pde_matching/envs/impact_plate/impact_plate.py
+++ src/pde_matching/envs/impact_plate/impact_plate.py
@@ -708,4 +708,4 @@ class ImpactPlateNormEnv(ImpactPlateEnv):
     
     @property
     def interesting_indices(self) -> List[int]:
-        return [1, 1, 2, 3, 4, 5, 6, 7, 8, 9]  # First 10 nodes
+        return [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]  # First 10 nodes
